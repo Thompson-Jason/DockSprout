b5c05a1190344e43adfae43a04aeb120a3c928f0
Jason Thompson <jason@jasonthompson.org>
2025-01-29 21:31:43 -0500

    Adding unit test to check if .compose-ignore is working

--- tests/walkerTests.rs
+++ tests/walkerTests.rs
@@ -3,6 +3,7 @@ mod tests {
     use std::fs::{create_dir_all, File};
     use tempfile::tempdir;
     use dock_sprout::walker::get_compose_filepaths;
+    use std::io::Write;
 
     #[test]
     fn test_find_docker_compose_files() {
@@ -18,9 +19,35 @@ mod tests {
         // Call function and check results
         let found_files = get_compose_filepaths(temp_dir.path());
 
-//        assert!(found_files.contains(&compose_file_path), "docker-compose.yml should be detected");
         assert!(found_files.contains(&compose_file_path.to_string_lossy().to_string()), 
             "docker-compose.yml should be detected");
+    }
+
+    #[test]
+    fn test_compose_ignore(){
+
+        // Create a temporary directory
+        let temp_dir = tempdir().unwrap();
+        let docker_dir = temp_dir.path().join("project1");
+        let ignore_dir = temp_dir.path().join("project2");
+        create_dir_all(&docker_dir).unwrap();
+        create_dir_all(&ignore_dir).unwrap();
+
+        // Create a dummy docker-compose.yml file
+        let compose_file_path1 = docker_dir.join("docker-compose.yml");
+        let compose_file_path2 = ignore_dir.join("docker-compose.yml");
+
+        // Create a dummy .compose-ignore file
+        let ignore_file_path = temp_dir.path().join(".compose-ignore");
+
+        File::create(&compose_file_path1).unwrap();
+        File::create(&compose_file_path2).unwrap();
+        File::create(&ignore_file_path).unwrap().write_all(b"project2/*");
+
+        // Call function and check results
+        let found_files = get_compose_filepaths(temp_dir.path());
+
+        assert!(!found_files.contains(&compose_file_path2.to_string_lossy().to_string()));
     }
 }
 
