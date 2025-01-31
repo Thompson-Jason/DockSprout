47ee4ff738870d0bf5ff58de6767b8e219f4455d
Jason Thompson <jason@jasonthompson.org>
2025-01-30 20:50:11 -0500

    Updating Command to use status instead of output

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,11 @@ pub mod walker;
 
 pub fn run_docker_compose<F>(files: Vec<String>, direction_args: Vec<String>, mut command_runner: F)
 where
-    F: FnMut(&str, &Vec<String>) -> std::io::Result<std::process::Output>,
+    F: FnMut(&str, &Vec<String>) -> std::io::Result<std::process::ExitStatus>,
 {
     for file_path in files.iter() {
 
-        let output = command_runner(&file_path, &direction_args).unwrap();
-
-        let stdout = String::from_utf8(output.stdout).unwrap();
-        println!("stdout {}", stdout);
-
-        let stderr = String::from_utf8(output.stderr).unwrap();
-        println!("stderr {}", stderr);
-
-        println!("status {}", output.status);
+        command_runner(&file_path, &direction_args).unwrap();
     }
 }
 
